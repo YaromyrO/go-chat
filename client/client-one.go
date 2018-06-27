da3c48c991b7444442b05f161a78d6d4a6e3e7f6
YaromyrO <oryshchyn97@gmail.com>
2018-06-27 21:37:31 +0300

    Func to anonim func, errors check

--- client/client-one.go
+++ client/client-one.go
@@ -2,28 +2,36 @@ package main
 
 import (
 	"net"
-	"fmt"
 	"bufio"
 	"os"
+	"fmt"
 )
-
-func listenResponse(connection net.Conn)  {
-	for {
-		response := make([]byte, 1024)
-		n, _ := connection.Read(response)
-		fmt.Print(string(response[:n]))
-	}
-}
 
 func main() {
 
-	connection, _ := net.Dial("tcp", "127.0.0.1:8080")
+	connection, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	go listenResponse(connection)
+	go func() {
+		for {
+			response := make([]byte, 1024)
+			n, err := connection.Read(response)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Print(string(response[:n]))
+		}
+	}()
 
 	for {
 		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
 		connection.Write([]byte(text))
 	}
 }
